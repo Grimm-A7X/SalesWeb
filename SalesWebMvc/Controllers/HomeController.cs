ecd11b74e17bd44de39ae195b37f55023daebea4
Christian Souza <christianyugioh@gmail.com>
2021-07-28 20:13:11 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales web MVC App from C# Course.";
+            ViewData["Student"] = "Christian Souza";
 
             return View();
         }
